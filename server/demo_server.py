781fbd39d47b93d7efd39fa0b50fdc47c7a83c74
hailey <jeong402@gmail.com>
2018-12-01 20:37:24 -0500

    created contact form

---
+++ server/demo_server.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template_string, render_template, Markup
+
+app = Flask(__name__, template_folder='template')
+
+@app.route('/', methods=['GET'])
+def welcome(): 
+	return "<h1> welcome to our demo server!</h1>"
+
+@app.route('/contact-info', methods=['GET', 'POST'])
+def contact_info(): 
+	if request.method == "GET": 
+		template = "contact.html"
+		return render_template(template)
+
+@app.route('/contact_submit', methods=['GET', 'POST'])
+def contact_submit(): 
+	template = "contact-submit.html"
+	if request.method == "POST":
+		
+		print(request.args)
+		print(request.form.get('name'))
+		name = request.form.get('name')
+		return render_template(template, name=name)
+
+	return render_template(template)
+
+
+####
+# Private function if the user has local files.
+###
+# def get_user_file(f_name):
+# 	with open(f_name) as f:
+# 		return f.readlines()
+
+
+# app.jinja_env.globals['get_user_file'] = get_user_file # Allows for use in Jinja2 templates
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000, debug=True)
+	
+
