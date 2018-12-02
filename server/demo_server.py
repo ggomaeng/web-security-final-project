a28b372677e448ae53631ea497c3aad5e0d58469
hailey <jeong402@gmail.com>
2018-12-01 21:49:12 -0500

    added home page

--- server/demo_server.py
+++ server/demo_server.py
@@ -4,7 +4,12 @@ app = Flask(__name__, template_folder='template')
 
 @app.route('/', methods=['GET'])
 def welcome(): 
-	return "<h1> welcome to our demo server!</h1>"
+	template = "home.html"
+	if request.args.get('search'):
+		search = request.args.get('search')
+		return render_template_string(''' <h1>Sorry No results on %s.  </h1>''' % (search) )
+	return render_template(template)
+
 
 @app.route('/contact-info', methods=['GET', 'POST'])
 def contact_info(): 
@@ -16,9 +21,6 @@ def contact_info():
 def contact_submit(): 
 	template = "contact-submit.html"
 	if request.method == "POST":
-		
-		print(request.args)
-		print(request.form.get('name'))
 		name = request.form.get('name')
 		return render_template(template, name=name)
 
