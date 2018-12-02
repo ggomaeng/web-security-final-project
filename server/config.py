fbdca0e36a0d4c09c6e5af76c94bc9cda054e1e2
hailey <jeong402@gmail.com>
2018-12-01 23:36:26 -0500

    added config file

---
+++ server/config.py
@@ -0,0 +1,6 @@
+
+class BaseConfig(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "#'\xfa\xd3-m.\x9c!\xbe\x15\xd3X\xa8O\xc2G\xc9u\x1e\xea*\x13\x7f"
